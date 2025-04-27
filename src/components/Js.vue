6bf9119e1c8496a06156180cd846af3557de29e7
Mery Efimova <d182840@rambler.ru>
2025-04-27 23:50:29 +0300

    Исправлена структура проекта после переинициализации

--- src/components/Js.vue
+++ src/components/Js.vue
@@ -1,6 +1,18 @@
-<script setup>
-
-</script>
 <template>
-    <div ref="boxContainer" class="box-container"></div>
+    <div>
+        <header>
+            <h2>JS animation</h2>
+            <div>
+                <router-link to="/"><button id="button-two">CSS анимация</button></router-link>
+                <router-link to="/anime"><button id="button-two">AnimeJS</button></router-link>
+                <router-link to="/gsap"><button id="button-two">GSAP</button></router-link>
+
+            </div>
+        </header>
+        <BoxContainer />
+    </div>
 </template>
+
+<script setup>
+import BoxContainer from '@/components/BoxContainer.vue'
+</script>
